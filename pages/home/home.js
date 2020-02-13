931ddf0716171cfc528b2f3a7c15a0bcd32045c9
1976868977@qq.com <1976868977@qq.com>
2020-02-13 19:43:26 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Hello Coderwhy',
+    age:18,
+    count:0,
+    movies: [
+      { id:1, name: '美国队长', year: 2018 },
+      { id:2, name: '钢铁侠', year: 1997 },
+      { id:3, name: '蜘蛛侠', year: 2019 }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  addcountClick(){
+    this.setData({
+      count: this.data.count+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  delcountClick() {
+    this.setData({
+      count: this.data.count-1
+    })
   }
 })

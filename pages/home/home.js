a9ecd4aeb67fee77bd805c4fc32f4f5f91dba63c
1976868977@qq.com <1976868977@qq.com>
2020-02-16 14:53:54 +0800

    加载页面常用配置

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("onLoad")
+    //模拟服务器返回的数据
+    const data = [{ name: '甄铭', age: 18 }, {name: '李文涛', age: 23 }]
+    //初始化数据
+    this.setData({
+      list:data
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("onShow")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("onUnload")
   },
-
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * 监听页面滚动
    */
-  onPullDownRefresh: function () {
-
+  onPageScroll: function(sec){
+      console.log(sec)
   },
-
   /**
-   * 页面上拉触底事件的处理函数
+   * 监听上拉到底部
    */
-  onReachBottom: function () {
-
+  onReachBottom(){
+      console.log('已经没有更多内容了')
   },
-
   /**
-   * 用户点击右上角分享
+   * 监听下拉
    */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log('下拉动作')
   }
+  /**
+   * 3. 监听wxml中发生的事件
+   */
 })
